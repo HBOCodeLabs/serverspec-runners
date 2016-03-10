cea541381399478f6319b6c7b0a8eb6ca14a8d13
Neill Turner <neillwturner@gmail.com>
2016-03-10 09:27:50 +0000

    ansiblespec runner

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # serverspec-runners
-Runner scripts for kitchen-verifier-serverspec
+
+Runner scripts for kitchen-verifier-serverspec.
+
+These files do enhanced functionality for serverspec testing in test kitchen when you set in the kitchen.yaml file
+```
+runner_url: https://raw.githubusercontent.com /neillturner/serverspec-runners/master/ansiblespec_runner.rb
+require_runner: true
+```
+By default test kitchen will always download and use the latest version of this install file.
+
+This script will install the puppet on a server. It defaults to puppet 3.x
+
+WARNING: AS SOON AS YOU MERGE CODE HERE IT IS INSTANTLY AVAILABLE TO EVERYONE USING THE RUNNER FROM KITCHEN-VERIFIER-SERVERSPEC:
+
+To get a previous version or lock the install script to a particular version update your kitchen.yaml file to:
+```
+runner_url: https://raw.githubusercontent.com/neillturner/serverspec-runners/ab2fa5771b6ef357786d7ff57d83fc8c97c22fed/ansiblespec_runner.rb
+require_runner: true
+```
+where ab2fa5771b6ef357786d7ff57d83fc8c97c22fed is the commit sha of the code.
+
+
+License
+---
+```
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ ansiblespec_runner.rb
@@ -0,0 +1,111 @@
+#
+# ansiblespec runner 
+#
+$stderr.sync = true
+require 'optparse'
+#require 'rake'
+#require 'rspec/core/rake_task'
+require 'rbconfig'
+require 'yaml'
+# default options
+config = {}
+config[:color] = false
+config[:format] = "documentation"
+config[:default_path] = ""
+version = false
+
+# parse arguments
+# ignore -P argument 
+file = __FILE__
+ARGV.options do |opts|
+  opts.on("-c", "--color")              { config[:color] = true }
+  opts.on("-f", "--format FORMATTER", String) { |val| config[:format] = val }
+  opts.on("", "--default-path PATH", String) { |val| config[:default_path] = val } 
+  opts.on("-v", "--version")              { version = true }
+  opts.on("-P", "")              { }
+  opts.parse!
+end
+
+base_path = Dir.pwd
+playbook = 'default.yml'
+playbook = ENV['PLAYBOOK'] if  ENV['PLAYBOOK']
+inventoryfile = 'hosts'
+inventoryfile = ENV['INVENTORY'] if  ENV['INVENTORY']
+kitchen_path = '/tmp/kitchen'
+kitchen_path = ENV['KITCHEN_PATH'] if  ENV['KITCHEN_PATH']
+user = 'root'
+user = ENV['LOGIN_USER'] if ENV['LOGIN_USER']
+sudo = ENV['SUDO'] if ENV['SUDO']
+ssh_key = nil
+if ENV['SSH_KEY']
+  s = ENV['SSH_KEY']
+  if s.start_with?('/') || s.start_with?('~')
+    ssh_key = s
+  else
+    # if it was passed in by kitchen-ansible provisioner assume it has been copied to the .ssh directory by the converge
+    ssh_key = "#{File.join("/home/#{user}/.ssh", File.basename(s))}"
+  end
+end
+login_password = nil
+login_password = ENV['LOGIN_PASSWORD'] if ENV['LOGIN_PASSWORD']
+
+puts "BASE_PATH: #{base_path}, KITCHEN_PATH #{kitchen_path}, PLAYBOOK: #{playbook}, INVENTORY: #{inventoryfile}, LOGIN_USER: #{user}, SSH_KEY: #{ssh_key}, LOGIN_PASSWORD: #{login_password}"
+
+if File.exist?("#{kitchen_path}/#{playbook}") == false
+  puts "Error: #{playbook} is not Found at #{kitchen_path}."
+  exit 1
+elsif File.exist?("#{kitchen_path}/#{inventoryfile}") == false
+  puts "Error: #{inventoryfile} is not Found at #{kitchen_path}."
+  exit 1
+end
+
+playbook_file = YAML.load_file("#{kitchen_path}/#{playbook}")
+properties = {}
+keys = 0
+
+playbook_file.each do |item|
+  ansible_hosts = item['hosts'].split(',')
+  ansible_roles = item['roles']
+  hostnames = false
+  ansible_hosts.each do |h|
+    begin
+      `ansible #{h} --list-hosts -i #{kitchen_path}/#{inventoryfile}`.lines do |line|
+        keys += 1
+        properties["host_#{keys}"] = {:host => line.strip, :roles => ansible_roles}
+        puts "group: #{h} host: #{line.strip!} roles: #{ansible_roles}"
+        hostnames = true
+      end
+    rescue
+    end
+    if !hostnames
+      keys += 1
+      properties["host_#{keys}"] = {:host => h, :roles => ansible_roles}
+      puts "no group so using host: #{h} roles: #{ansible_roles}"
+    end
+  end
+end
+
+# Environment variable TARGET_HOST, LOGIN_USER, LOGIN_PASSWORD, SSH_KEY are specified in the spec_helper  
+ENV['LOGIN_USER'] = user
+ENV['SSH_KEY'] = ssh_key
+
+rspec_path = ''
+if sudo == 'true' 
+  rspec_cmd = " sudo -E #{rspec_path}rspec"
+else
+  rspec_cmd = "#{rspec_path}rspec"
+end 
+
+properties.keys.each do |key|
+  #desc "Run serverspec #{key} for #{properties[key][:host]}"
+  puts "-----> Run serverspec #{key} for host: #{properties[key][:host]} roles: #{properties[key][:roles]}" 
+  # Environment variable TARGET_HOST, LOGIN_USER, LOGIN_PASSWORD, SSH_KEY are specified in the spec_helper 
+  ENV['TARGET_HOST'] = properties[key][:host]
+  s = "#{kitchen_path}/roles/{" + properties[key][:roles].join(',') + '}/spec/*_spec.rb'
+  color = nil 
+  color = '-c' if config[:color] 
+  puts "-----> Running: #{rspec_cmd} #{color} -f #{config[:format]} --default-path  #{config[:default_path]} -P #{s}"
+  system "#{rspec_cmd} #{color} -f #{config[:format]} --default-path  #{config[:default_path]} -P #{s}" 
+end
+
+ 
